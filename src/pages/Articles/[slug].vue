e680c6791f873329d1ca667ddf6cb48e7fc306f6
somed00d <41293874+somed00d@users.noreply.github.com>
2021-12-19 07:06:29 -0500

    Add Article index and slug page

---
+++ src/pages/Articles/[slug].vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { useGhostStore } from '~/stores/ghost'
+const ghostStore = useGhostStore()
+const article = computed(() => ghostStore.listCurrentArticle)
+</script>
+
+<template>
+  <article>
+    <h1>{{ article?.title }}</h1>
+    <div class="article-wrapper">
+      <div class="article-content" v-html="article?.html" />
+    </div>
+  </article>
+</template>
+
+<style lang="postcss" scoped>
+.article-wrapper {
+  border: 1px solid blue;
+  /*  */
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  width: 65ch;
+  position: relative;
+
+  & ::v-deep(.article-content) {
+    width: 65ch;
+    font-family: 'Noto Serif';
+    font-weight: 400;
+    & img {
+      width: 65ch;
+    }
+
+    & figure {
+      margin: 0;
+    }
+  }
+}
+</style>
+
+<route lang="yaml">
+meta:
+  layout: article
+</route>
--- src/pages/Articles/index.vue
+++ src/pages/Articles/index.vue
@@ -4,9 +4,8 @@
 <template>
   <div class="articles-page-wrapper">
     <the-headlines-marquee />
-    <articles-featured-four-box />
-    <articles-other-box />
-    <articles-tag-box />
-    <articles-articles-box />
+    <articles-featured-four />
+    <articles-opinion />
+    <articles-latest />
   </div>
 </template>
