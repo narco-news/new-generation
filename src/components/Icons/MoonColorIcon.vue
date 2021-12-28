be1dcab2381d57d1d320c39348645694a90e7ad3
somed00d <41293874+somed00d@users.noreply.github.com>
2021-12-28 15:10:38 -0500

    Add icon components

---
+++ src/components/Icons/MoonColorIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="36"
+    height="36"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ><path d="M19,16A9,9,0,0,1,10.91,3.07a9,9,0,1,0,9.18,12.86A9.81,9.81,0,0,1,19,16Z" fill="#17a578" opacity="0.2" stroke-width="0" /><path d="M19,16A9,9,0,0,1,10.91,3.07a9,9,0,1,0,9.18,12.86A9.81,9.81,0,0,1,19,16Z" /></svg>
+</template>
+
+<style lang="postcss" scoped>
+svg {
+  width: 18px;
+  height: 18px;
+}
+</style>
---
+++ src/components/Icons/SunColorIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="36"
+    height="36"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ><circle
+    cx="12"
+    cy="12"
+    r="4"
+    fill="#17a578"
+    opacity="0.1"
+    stroke-width="0"
+  /><circle cx="12" cy="12" r="4" /><line x1="12" y1="2" x2="12" y2="4" /><line x1="12" y1="20" x2="12" y2="22" /><line x1="22" y1="12" x2="20" y2="12" /><line x1="4" y1="12" x2="2" y2="12" /><line x1="19.07" y1="19.07" x2="17.66" y2="17.66" /><line x1="6.34" y1="6.34" x2="4.93" y2="4.93" /><line x1="4.93" y1="19.07" x2="6.34" y2="17.66" /><line x1="17.66" y1="6.34" x2="19.07" y2="4.93" /></svg>
+</template>
+
+<style lang="postcss" scoped>
+svg {
+  width: 18px;
+  height: 18px;
+}
+</style>
