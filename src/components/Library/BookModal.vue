25569306acdce3e8b79ce2d45cdf229294260341
somed00d <41293874+somed00d@users.noreply.github.com>
2022-01-08 08:45:38 -0500

    Add library page

---
+++ src/components/Library/BookModal.vue
@@ -0,0 +1,154 @@
+<script setup lang="ts">
+defineEmits(['close'])
+</script>
+<template>
+  <div class="modal-mask">
+    <div
+      class="modal-wrapper"
+    >
+      <div
+        class="modal-container"
+      >
+        <button class="modal-close-button" @click="$emit('close')">
+          <XIcon />
+        </button>
+        <slot class="slotted" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+
+::v-deep(.modal-container) {
+  img {
+    object-fit: contain;
+    aspect-ratio: 1/1;
+    max-height: 300px;
+    @media (max-width: 425px) {
+      max-height: 200px;
+      margin-top: 3rem;
+    }
+  }
+  p {
+    font-weight: 500;
+    font-size: clamp(100%, 11px + 2vw, 16px);
+    font-style: italic;
+  }
+  .modal-author {
+    color: var(--green);
+    font-weight: 400;
+  }
+  .modal-meta {
+    padding: 10px;
+    @media (min-width: 426px) {
+      padding: 2rem;
+    }
+  }
+  p.modal-desc {
+    font-family: 'Noto Serif';
+    @media (max-width: 425px) {
+      font-size: 13px;
+      margin: 0;
+    }
+  }
+}
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(255, 255, 255, 0.7);
+  backdrop-filter: blur(12px);
+  /* background-color: var(--slate-100); */
+  display: table;
+
+  @supports not (backdrop-filter: blur(7px)) {
+    background-color: var(--slate-50);
+  }
+}
+
+.modal-wrapper {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  margin: 0 auto;
+  max-width: 1000px;
+  @media (max-width: 425px) {
+    align-items: flex-start;
+  }
+}
+
+.modal-container {
+  margin: 0 auto;
+  padding: 1.5rem clamp(1rem, 5%, 3rem);
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+}
+
+.modal-image {
+    img {
+        max-width: 100px;
+    }
+}
+
+.modal-close-button {
+  display: flex;
+  align-items: center;
+  background: none;
+  border: none;
+  position: absolute;
+  top: 5px;
+  right: 0;
+  /* @media (max-width: 425px) {
+    display: none;
+  } */
+  svg {
+    height: 18px;
+    width: 18px;
+  }
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
+
+html.dark {
+  .modal-close-button {
+    color: white;
+  }
+
+  .modal-mask {
+    background-color: rgba(0, 0, 0, 0.534);
+    @supports not (backdrop-filter: blur(7px)) {
+      background-color: #161618;
+      color: white;
+    }
+  }
+}
+</style>
--- src/pages/Library.vue
+++ src/pages/Library.vue
@@ -1,7 +1,129 @@
-
 <script setup lang="ts">
-useHead({
-  title: 'Library',
-})
+import booksList from '../assets/book-list.json'
+const { t } = useI18n({ useScope: 'global' })
+const showModal = ref(false)
+const modalBook = ref({})
+function createModal(book) {
+  modalBook.value = book
+  showModal.value = true
+}
 </script>
-<template><h1>Library</h1></template>
+
+<template>
+  <section class="library__wrapper">
+    <h1 class="library__header">
+      {{ t('nav.library') }}
+    </h1>
+    <i18n-t global keypath="library.intro" tag="p" class="library__intro">
+      <template #book>
+        <span style="font-style:italic;font-weight:500">{{ t('library.narcoHistory') }}</span>
+      </template>
+    </i18n-t>
+    <div class="library__books-wrapper">
+      <div v-for="book in booksList.books" :key="book.id">
+        <BookCard :book="book" @open-modal="createModal($event)" />
+      </div>
+      <transition name="modal">
+        <book-modal
+          v-if="showModal"
+          @close="showModal = false"
+          @wheel.prevent
+          @touchmove.prevent
+          @scroll.prevent
+        >
+          <div style="display:flex;">
+            <img :src="modalBook.imageUrl" :alt="modalBook.title">
+            <div
+              class="modal-meta"
+            >
+              <h1>
+                {{ modalBook.title }}
+              </h1>
+              <h2 class="modal-author">
+                {{ modalBook.authors }}
+              </h2>
+              <p class="modal-date">
+                {{ modalBook.datePublished }}
+              </p>
+            </div>
+          </div>
+          <p class="modal-desc">
+            "{{ modalBook.desc }}"
+          </p>
+        </book-modal>
+      </transition>
+    </div>
+  </section>
+</template>
+
+<style lang="postcss" scoped>
+.library__wrapper {
+  .library__header {
+    color: black;
+    margin: 10px;
+  }
+
+  .library__intro {
+    margin: 10px;
+    font-size: 13px;
+    max-width: 65ch;
+  }
+  .library__books-wrapper {
+    /* first breakpoint*/
+    --w1:1200px;
+    --n:5;
+    /* second breakpoint*/
+    --w2:768px;
+    --m:4;
+    /* third breakpoint*/
+    --w3:400px;
+    --p:2;
+    margin: 2rem 2rem;
+
+    display:grid;
+    grid-template-columns:
+    repeat(auto-fill,
+    minmax(clamp(clamp(clamp(
+            100%/(var(--n) + 1) + 0.1%,
+              (var(--w1) - 100vw)*1000,
+            100%/(var(--m) + 1) + 0.1%),
+              (var(--w2) - 100vw)*1000,
+            100%/(var(--p) + 1) + 0.1%),
+              (var(--w3) - 100vw)*1000,
+            100%), 1fr));
+    gap: 20px;
+  }
+}
+
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
+
+html.dark {
+  .library__header {
+    color: white;
+  }
+  .library__intro {
+    color: white;
+  }
+  .modal-container {
+    .modal-meta {
+      h1 {
+        color: white;
+      }
+      .modal-date {
+        color: white;
+      }
+    }
+    .modal-desc {
+      color: white
+    }
+  }
+}
+</style>
