15fe6d5681a43a9efee9202dce4ff3ca49b521b9
somed00d <41293874+somed00d@users.noreply.github.com>
2022-02-08 01:58:22 -0500

    Clean up articles page

--- src/pages/Articles/index.vue
+++ src/pages/Articles/index.vue
@@ -4,19 +4,15 @@ const useGhost = useGhostStore()
 useHead({
   title: 'Articles',
 })
-const featuredArticles = computed(() => useGhost.listFeaturedFourArticles)
 const latestArticles = computed(() => useGhost.listLatestArticles)
 </script>
 
 <template>
-  <template v-if="featuredArticles">
-    <articles-featured
-      :articles="featuredArticles"
-    />
-  </template>
-  <div v-if="latestArticles">
-    <articles-latest
-      :articles="latestArticles"
-    />
-  </div>
+  <section>
+    <div v-if="latestArticles">
+      <articles-latest
+        :articles="latestArticles"
+      />
+    </div>
+  </section>
 </template>
