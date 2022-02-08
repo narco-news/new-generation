462477c99c8c0a208b40e4edea2521b3290b551c
somed00d <41293874+somed00d@users.noreply.github.com>
2022-02-07 23:13:08 -0500

    Adjust Tags page header

--- src/pages/Tags/[slug].vue
+++ src/pages/Tags/[slug].vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useMotion } from '@vueuse/motion'
 import axios from 'axios'
 import { useGhostStore } from '~/stores/ghost'
 const route = useRoute()
@@ -35,16 +36,34 @@ watchEffect(() => {
     //   window.scroll(0, 0)
     // })
 })
+const { y } = useWindowScroll()
+//
+// Scroll to top button
+function scrollTop() {
+  const intervalId = setInterval(() => {
+    if (window.pageYOffset === 0) clearInterval(intervalId)
+
+    window.scroll(0, window.pageYOffset - 50)
+  }, 20)
+}
 </script>
 <template>
   <section>
     <div v-if="tag" class="tag-wrapper">
-      <h1 class="tag-name">
-        {{ tag.name }}
-      </h1>
-      <h2 class="tag-count">
-        {{ tag.count.posts }} articles
-      </h2>
+      <div style="display:flex;align-items:baseline">
+        <h1 class="tag-name">
+          {{ tag.name }}
+        </h1>
+        <h2 class="tag-count">
+          {{ tag.count.posts }} articles
+        </h2>
+      </div>
+      <div
+        v-if="y > 4000"
+        @click="scrollTop()"
+      >
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.14645 2.14645C7.34171 1.95118 7.65829 1.95118 7.85355 2.14645L11.8536 6.14645C12.0488 6.34171 12.0488 6.65829 11.8536 6.85355C11.6583 7.04882 11.3417 7.04882 11.1464 6.85355L8 3.70711L8 12.5C8 12.7761 7.77614 13 7.5 13C7.22386 13 7 12.7761 7 12.5L7 3.70711L3.85355 6.85355C3.65829 7.04882 3.34171 7.04882 3.14645 6.85355C2.95118 6.65829 2.95118 6.34171 3.14645 6.14645L7.14645 2.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" /></svg>
+      </div>
     </div>
     <div v-if="tagArticles">
       <keep-alive>
@@ -84,7 +103,18 @@ watchEffect(() => {
 .tag-wrapper {
   display: flex;
   align-items: baseline;
+  justify-content: space-between;
   padding: 0 10px;
+  position: sticky;
+  position: -webkit-sticky;
+  top: 0;
+  z-index: 100;
+  background-color: var(--slate-100);
+  border-bottom: 2px solid var(--slate-600);
+  margin: 0 1rem;
+  @media (min-width: 768px) {
+    margin: 0;
+  }
 }
 .tag-count {
   font-weight: 400;
@@ -97,6 +127,9 @@ watchEffect(() => {
 }
 
 html.dark {
+  .tag-wrapper {
+    background-color: black;
+  }
   .tag-name {
     color: white;
   }
