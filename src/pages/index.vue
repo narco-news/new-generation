57779177e6092f4b8711e9f15cc7ee86374db1ae
somed00d <41293874+somed00d@users.noreply.github.com>
2022-02-08 21:33:20 -0500

    Finalize home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -30,12 +30,45 @@ const opinionArticles = computed(() => useGhost.listOpinionArticles)
       />
     </div>
     <template v-if="opinionArticles.length">
-      <articles-opinion
-        :articles="opinionArticles"
-      />
+      <div ref="opinionArticlesEl">
+        <articles-opinion
+          :articles="opinionArticles"
+        />
+      </div>
     </template>
+    <div
+      class="trans-atw"
+    >
+      <template
+        v-if="translationArticles.length"
+      >
+        <latest-box
+          heading="Translations"
+          more-button="Translations"
+          slug="translation"
+          :articles="translationArticles"
+          :show-image="false"
+        />
+      </template>
+      <template v-if="aroundTheWebArticles.length">
+        <latest-box
+          heading="Around the Web"
+          slug="around-the-web"
+          more-button="ATW"
+          :articles="aroundTheWebArticles"
+          :show-image="true"
+        />
+      </template>
+    </div>
   </section>
 </template>
 
 <style lang="postcss" scoped>
+.trans-atw {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+}
 </style>
