f70401ada525498f3925eef28e64d8fd81c22afb
somed00d <41293874+somed00d@users.noreply.github.com>
2022-02-09 04:50:40 -0500

    Clean up icons

--- src/components/Icons/ExternalLinkIcon.vue
+++ src/components/Icons/ExternalLinkIcon.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  border?: boolean
+}>()
+const iconBoxShadow = ref()
+const HOVER_iconBoxShadow = ref()
+if (props.border === true) {
+  iconBoxShadow.value = '0 0 0 1px var(--slate-400)'
+  HOVER_iconBoxShadow.value = '0 0 0 2px var(--green)'
+}
+else {
+  iconBoxShadow.value = 'none'
+  HOVER_iconBoxShadow.value = 'none'
+}
+</script>
+
 <template>
   <div class="external-link-icon">
     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" /></svg>
@@ -8,12 +24,15 @@
 .external-link-icon {
     display: grid;
     place-content: center;
-    border: 1px solid black;
     border-radius: 8px;
     padding: 6px;
     margin: 0 5px;
+    box-shadow: v-bind(iconBoxShadow);
+    transition: all 180ms ease-in;
+    color: inherit;
     &:hover {
-      box-shadow: 0 0 0 1px var(--green);
+      color: inherit;
+      box-shadow: v-bind(HOVER_iconBoxShadow);
       border-color: transparent;
     }
     svg {
@@ -24,8 +43,8 @@
 
 html.dark {
   .external-link-icon {
-    color: white;
-    border-color: white;
+    color: inherit;
+    box-shadow: 0 0 0 1px var(--slate-600);
   }
 }
 </style>
--- src/components/Icons/MoonColorIcon.vue
+++ src/components/Icons/MoonColorIcon.vue
@@ -2,14 +2,14 @@
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    width="36"
-    height="36"
+    width="24"
+    height="24"
     fill="none"
     stroke="currentColor"
-    stroke-width="1"
+    stroke-width="1.5"
     stroke-linecap="round"
     stroke-linejoin="round"
-  ><path d="M19,16A9,9,0,0,1,10.91,3.07a9,9,0,1,0,9.18,12.86A9.81,9.81,0,0,1,19,16Z" fill="#17a578" opacity="0.2" stroke-width="0" /><path d="M19,16A9,9,0,0,1,10.91,3.07a9,9,0,1,0,9.18,12.86A9.81,9.81,0,0,1,19,16Z" /></svg>
+  >&lt;!--!  Atomicons Free 1.00 by @atisalab License - https://atomicons.com/license/ (Icons: CC BY 4.0) Copyright 2021 Atomicons --&gt;<path d="M19,16A9,9,0,0,1,10.91,3.07a9,9,0,1,0,9.18,12.86A9.81,9.81,0,0,1,19,16Z" fill="#000000" opacity="1" stroke-width="0" /><path d="M19,16A9,9,0,0,1,10.91,3.07a9,9,0,1,0,9.18,12.86A9.81,9.81,0,0,1,19,16Z" /></svg>
 </template>
 
 <style lang="postcss" scoped>
